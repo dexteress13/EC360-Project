924ae3ab26b37ca77160fabd67937c72983d0e52
ShaflaRehman <shaflarehman001@gmail.com>
2026-03-28 20:40:18 +0500

    EP-3: register paper routes in express server

--- server/index.js
+++ server/index.js
@@ -14,6 +14,8 @@ const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
 const reviewerRoutes = require('./routes/reviewer');
 app.use('/api/reviewer', reviewerRoutes);
+const paperRoutes = require('./routes/paper');
+app.use('/api/paper', paperRoutes);
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
